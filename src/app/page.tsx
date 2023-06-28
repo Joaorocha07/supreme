42f073e9cf5ede34a97ec5f811fb38d800efaf67
João Victor <rocha.joao.victor.50@gmail.com>
2023-06-28 10:58:22 -0300

    Criação do repositorio com coisas criada para supreme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,188 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+import { useState, useEffect } from "react";
+import { AppBar, Box, Card, Container, CssBaseline, GlobalStyles, Grid, ThemeProvider, Toolbar, Typography, createTheme } from "@mui/material";
+import { eachDayOfInterval, getDay } from "date-fns";
+
+import fundo from '../../public/fundo.png'
+import card1 from '../../public/card1.png'
+import card2 from '../../public/card2.png'
+import card3 from '../../public/card3.png'
+
+const defaultTheme = createTheme();
 
 export default function Home() {
+  const [treinosFaltantes, setTreinosFaltantes] = useState(0);
+
+  const concluirTreino = () => {
+    setTreinosFaltantes(treinosFaltantes - 1);
+  };
+
+  useEffect(() => {
+    const hoje = new Date();
+    const dataCheerfest = new Date("2023-12-02"); // Data do Cheerfest (2 de dezembro de 2023)
+
+    const domingos = eachDayOfInterval({ start: hoje, end: dataCheerfest }).filter(
+      (date) => getDay(date) === 0
+    );
+
+    setTreinosFaltantes(domingos.length);
+
+    const timer = setInterval(() => {
+      const currentDate = new Date();
+      // Verifica se é domingo (0 representa domingo, 1 representa segunda-feira e assim por diante)
+      if (currentDate.getDay() === 0) {
+        concluirTreino();
+      }
+    }, 1000 * 60 * 60 * 24); // Verifica a cada 24 horas (1 dia)
+
+    return () => {
+      clearInterval(timer); // Limpa o timer quando o componente for desmontado
+    };
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+        <CssBaseline />
+        <Grid
+          sx={{
+            backgroundImage: `url(${fundo.src})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundAttachment: 'fixed',
+            height: '100vh',
+          }}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+        <AppBar
+          position="static"
+          color="default"
+          elevation={0}
+          sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
         >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+          <Toolbar sx={{ flexWrap: 'wrap' }}>
+            <Typography 
+              variant="h6" 
+              color="inherit" 
+              noWrap 
+              sx={{ 
+                flexGrow: 1,
+                textAlign: 'center',
+              }}
+            >
+              Cheer Supreme
+            </Typography>
+          </Toolbar>
+        </AppBar>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+        <Container disableGutters maxWidth="sm" component="main" sx={{ pt: 8, pb: 6 }}>
+          <Grid textAlign="center">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+              sx={{
+                color: "white",
+                padding: '10px',
+                borderRadius: '10px 10px 10px 10px',
+                background: "rgba(0, 0, 0, 0.5)",
+                '@media (max-width: 600px)': {
+                  color: 'white',
+                  padding: '2px',
+                  fontSize: '30px'
+                }, 
+              }}
+            >
+            Faltam {treinosFaltantes}
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" component="p" sx={{
+                color: "white",
+                padding: '10px',
+                borderRadius: '10px 10px 10px 10px',
+                background: "rgba(0, 0, 0, 0.5)", 
+                '@media (max-width: 600px)': {
+                  padding: '2px',
+                  fontSize: '30px',
+                  color: 'white'
+                }, 
+              }}>
+              Treinos para o campeonato CheerFest 
+            </Typography>
+          </Grid>
+        </Container>
+        
+        {/* <Container maxWidth="md" component="main">
+          <Grid container spacing={5} alignItems="center">
+              <Box
+                 sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  margin: "0 auto",
+                  '@media (max-width: 600px)': {
+                    flexDirection: 'column',
+                    marginRight: '-14px'
+                  },
+                }}
+              >
+                <Grid item xs={12} sm={6}>
+                  <Card
+                    sx={{
+                      height: "200px",
+                      width: "300px",
+                      margin: "8px",
+                      backgroundImage: `url(${card1.src})`,
+                      backgroundSize: 'cover',
+                      backgroundPosition: "center",
+                    }}
+                  >
+                  </Card>
+                </Grid>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+                <Grid item xs={12} sm={6}>
+                  <Card
+                    sx={{
+                      height: "300px",
+                      width: "200px",
+                      margin: "8px",
+                      backgroundImage: `url(${card2.src})`,
+                      backgroundSize: 'cover',
+                      backgroundPosition: "center",
+                    }}
+                  >
+                  </Card>
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                  <Card
+                    sx={{
+                      height: "200px",
+                      width: "300px",
+                      margin: "8px",
+                      backgroundImage: `url(${card3.src})`,
+                      backgroundSize: 'cover',
+                      backgroundPosition: "center",
+                    }}
+                  >
+                  </Card>
+                </Grid>
+              </Box>
+          </Grid>
+        </Container> */}
+
+        <Container
+          maxWidth="md"
+          component="footer"
+          sx={{
+            borderTop: (theme) => `1px solid ${theme.palette.divider}`,
+            mt: 8,
+            alignItems: 'center'
+          }}
         >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+
+        </Container>
+        </Grid>
+      </ThemeProvider>
   )
 }
